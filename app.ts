02036436719b568b0f44b3124597d56b77e6eeab
Jeferson Vinícius <jeferson.viniciuscrc@gmail.com>
2021-06-10 20:03:47 -0300

    Start spliting statement in phases

--- app.ts
+++ app.ts
@@ -2,20 +2,49 @@ import plays from './plays.json';
 import invoices from './invoices.json';
 import { Invoice, Performance, Play } from './types';
 
-function statement(invoice: Invoice, plays: { [key: string]: Play }) {
-    let result = `Statement for ${invoice.customer}\n`;
+type Plays = { [key: string]: Play };
 
-    for (let perf of invoice.performances) {
-        result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
+type StatementData = {
+    customer: string;
+    performances: PerformanceEnriched[];
+};
+
+type PerformanceEnriched = Performance & {
+    play: Play;
+};
+
+function statement(invoice: Invoice, plays: Plays) {
+    const statementData: StatementData = {
+        customer: invoice.customer,
+        performances: invoice.performances.map(enrichPerformance),
+    };
+    return renderPlainText(statementData, plays);
+
+    function enrichPerformance(performance: Performance) {
+        const result = Object.assign({}, performance) as PerformanceEnriched;
+        result.play = playFor(performance);
+        return result;
+    }
+
+    function playFor(performance: Performance) {
+        return plays[performance.playID];
+    }
+}
+
+function renderPlainText(data: StatementData, plays: Plays) {
+    let result = `Statement for ${data.customer}\n`;
+
+    for (let perf of data.performances) {
+        result += ` ${perf.play.name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
     }
 
     result += `Amount owed is ${usd(totalAmount())}\n`;
     result += `You earned ${totalVolumeCredits()} credits\n`;
     return result;
 
-    function amountFor(performance: Performance) {
+    function amountFor(performance: PerformanceEnriched) {
         let result = 0;
-        switch (playFor(performance).type) {
+        switch (performance.play.type) {
             case 'tragedy':
                 result = 40000;
                 if (performance.audience > 30) {
@@ -30,18 +59,14 @@ function statement(invoice: Invoice, plays: { [key: string]: Play }) {
                 result += 300 * performance.audience;
                 break;
             default:
-                throw new Error(`unknown type: ${playFor(performance).type}`);
+                throw new Error(`unknown type: ${performance.play.type}`);
         }
         return result;
     }
 
-    function playFor(performance: Performance) {
-        return plays[performance.playID];
-    }
-
-    function volumeCreditsFor(performance: Performance) {
+    function volumeCreditsFor(performance: PerformanceEnriched) {
         let result = Math.max(performance.audience - 30, 0);
-        if ('comedy' === playFor(performance).type) result += Math.floor(performance.audience / 5);
+        if ('comedy' === performance.play.type) result += Math.floor(performance.audience / 5);
         return result;
     }
 
@@ -55,7 +80,7 @@ function statement(invoice: Invoice, plays: { [key: string]: Play }) {
 
     function totalVolumeCredits() {
         let result = 0;
-        for (let perf of invoice.performances) {
+        for (let perf of data.performances) {
             result += volumeCreditsFor(perf);
         }
         return result;
@@ -63,7 +88,7 @@ function statement(invoice: Invoice, plays: { [key: string]: Play }) {
 
     function totalAmount() {
         let result = 0;
-        for (let perf of invoice.performances) {
+        for (let perf of data.performances) {
             result += amountFor(perf);
         }
         return result;
