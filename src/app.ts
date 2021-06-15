f504b46a4d19552bf5d7bf9e3a58e6730edbefdc
Jeferson Vinícius <jeferson.viniciuscrc@gmail.com>
2021-06-15 18:57:58 -0300

    Create htmlStatement

--- src/app.ts
+++ src/app.ts
@@ -3,8 +3,33 @@ import invoices from '../invoices.json';
 import { Invoice } from './types';
 import { createStatementData, Plays, StatementData } from './createStatementData';
 
+function usd(value: number) {
+    return new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD',
+        minimumFractionDigits: 2,
+    }).format(value / 100);
+}
+
 function statement(invoice: Invoice, plays: Plays) {
     return renderPlainText(createStatementData(invoice, plays));
+}
+
+function htmlStatement(invoice: Invoice, plays: Plays) {
+    return renderHTML(createStatementData(invoice, plays));
+}
+
+function renderHTML(data: StatementData) {
+    let result = `<h1>Statement for ${data.customer}</h1>\n`;
+    result += '<table>\n';
+    result += '<tr><th>play</th><th>seats</th><th>cost</th></tr>\n';
+    for (let perf of data.performances) {
+        result += ` <tr><th>${perf.play.name}</th><th>${perf.audience}</th><th>${usd(perf.amount)}</th></tr>\n`;
+    }
+    result += '</table>\n';
+    result += `<p>Amount owed is <em>${usd(data.totalAmount)}</em></p>`;
+    result += `<p>You earned <em>${data.totalVolumeCredits}</em> credits</p>`;
+    return result;
 }
 
 function renderPlainText(data: StatementData) {
@@ -17,14 +42,6 @@ function renderPlainText(data: StatementData) {
     result += `Amount owed is ${usd(data.totalAmount)}\n`;
     result += `You earned ${data.totalVolumeCredits} credits\n`;
     return result;
-
-    function usd(value: number) {
-        return new Intl.NumberFormat('en-US', {
-            style: 'currency',
-            currency: 'USD',
-            minimumFractionDigits: 2,
-        }).format(value / 100);
-    }
 }
 const EXPECTED = `Statement for BigCo
  Hamlet: $650.00 (55 seats)
@@ -34,8 +51,13 @@ Amount owed is $1,730.00
 You earned 47 credits
 `;
 
+// for (const invoice of invoices) {
+//     const result = statement(invoice, plays);
+//     console.log(result);
+//     console.log(result === EXPECTED ? 'OK' : "Error: result doesn't expected");
+// }
+
 for (const invoice of invoices) {
-    const result = statement(invoice, plays);
+    const result = htmlStatement(invoice, plays);
     console.log(result);
-    console.log(result === EXPECTED ? 'OK' : "Error: result doesn't expected");
 }
