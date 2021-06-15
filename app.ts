0161ecc9c6741b8cf0a130cf4acb86405e592aff
Jeferson Vinícius <jeferson.viniciuscrc@gmail.com>
2021-06-15 18:40:51 -0300

    Replace loop with pipeline

--- app.ts
+++ app.ts
@@ -28,19 +28,11 @@ function statement(invoice: Invoice, plays: Plays) {
     return renderPlainText(statementData, plays);
 
     function totalVolumeCredits(performances: PerformanceEnriched[]) {
-        let result = 0;
-        for (let perf of performances) {
-            result += perf.volumeCredits;
-        }
-        return result;
+        return performances.reduce((total, p) => total + p.volumeCredits, 0);
     }
 
     function totalAmount(performances: PerformanceEnriched[]) {
-        let result = 0;
-        for (let perf of performances) {
-            result += perf.amount;
-        }
-        return result;
+        return performances.reduce((total, p) => total + p.amount, 0);
     }
 
     function enrichPerformance(performance: Performance) {
