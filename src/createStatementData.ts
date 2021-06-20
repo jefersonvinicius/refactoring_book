55c2fb84cea2ebaa6a448e4b66a1369fcc9d6149
Jeferson Vinícius <jeferson.viniciuscrc@gmail.com>
2021-06-20 17:40:38 -0300

    Moving amount conditionals to subclass

--- src/createStatementData.ts
+++ src/createStatementData.ts
@@ -25,31 +25,45 @@ class PerformanceCalculator {
         this.play = play;
     }
 
-    get amount() {
-        let result = 0;
-        switch (this.play.type) {
-            case 'tragedy':
-                result = 40000;
-                if (this.performance.audience > 30) {
-                    result += 1000 * (this.performance.audience - 30);
-                }
-                break;
-            case 'comedy':
-                result = 30000;
-                if (this.performance.audience > 20) {
-                    result += 10000 + 500 * (this.performance.audience - 20);
-                }
-                result += 300 * this.performance.audience;
-                break;
-            default:
-                throw new Error(`unknown type: ${this.play.type}`);
-        }
-        return result;
+    get amount(): number {
+        throw new Error('subclass responsibility');
     }
 
     get volumeCredits() {
         let result = Math.max(this.performance.audience - 30, 0);
         if ('comedy' === this.play.type) result += Math.floor(this.performance.audience / 5);
+        return result;
+    }
+}
+
+function createPerformanceCalculator(performance: Performance, play: Play) {
+    switch (play.type) {
+        case 'tragedy':
+            return new TragedyCalculator(performance, play);
+        case 'comedy':
+            return new ComedyCalculator(performance, play);
+        default:
+            throw new Error(`unknown type: ${play.type}`);
+    }
+}
+
+class TragedyCalculator extends PerformanceCalculator {
+    get amount() {
+        let result = 40000;
+        if (this.performance.audience > 30) {
+            result += 1000 * (this.performance.audience - 30);
+        }
+        return result;
+    }
+}
+
+class ComedyCalculator extends PerformanceCalculator {
+    get amount() {
+        let result = 30000;
+        if (this.performance.audience > 20) {
+            result += 10000 + 500 * (this.performance.audience - 20);
+        }
+        result += 300 * this.performance.audience;
         return result;
     }
 }
@@ -74,7 +88,7 @@ function totalAmount(performances: PerformanceEnriched[]) {
 }
 
 function enrichPerformance(performance: Performance) {
-    const calculator = new PerformanceCalculator(performance, playFor(performance));
+    const calculator = createPerformanceCalculator(performance, playFor(performance));
     const result = Object.assign({}, performance) as PerformanceEnriched;
     result.play = calculator.play;
     result.amount = calculator.amount;
