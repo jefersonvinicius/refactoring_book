4af1806251b236c09692309d30151460c79ec1e4
Jeferson Vinícius <jeferson.viniciuscrc@gmail.com>
2021-06-20 17:50:16 -0300

    Moving volumeCredits conditionals to subclass

--- src/createStatementData.ts
+++ src/createStatementData.ts
@@ -30,9 +30,7 @@ class PerformanceCalculator {
     }
 
     get volumeCredits() {
-        let result = Math.max(this.performance.audience - 30, 0);
-        if ('comedy' === this.play.type) result += Math.floor(this.performance.audience / 5);
-        return result;
+        return Math.max(this.performance.audience - 30, 0);
     }
 }
 
@@ -66,6 +64,10 @@ class ComedyCalculator extends PerformanceCalculator {
         result += 300 * this.performance.audience;
         return result;
     }
+
+    get volumeCredits() {
+        return super.volumeCredits + Math.floor(this.performance.audience / 5);
+    }
 }
 
 export function createStatementData(invoice: Invoice, plays: Plays) {
@@ -96,12 +98,6 @@ function enrichPerformance(performance: Performance) {
     return result;
 }
 
-function volumeCreditsFor(performance: PerformanceEnriched) {}
-
 function playFor(performance: Performance) {
     return plays[performance.playID];
-}
-
-function amountFor(performance: PerformanceEnriched) {
-    return new PerformanceCalculator(performance, playFor(performance)).amount;
 }
