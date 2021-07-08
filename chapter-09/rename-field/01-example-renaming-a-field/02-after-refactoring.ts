d46201a443984cdf0fc56c6b86db9cb4db7cc003
Jeferson Vinícius <jeferson.viniciuscrc@gmail.com>
2021-07-08 18:39:11 -0300

    Finished "rename field" example

--- chapter-09/rename-field/01-example-renaming-a-field/02-after-refactoring.ts
+++ chapter-09/rename-field/01-example-renaming-a-field/02-after-refactoring.ts
@@ -2,20 +2,41 @@
 // const organization = { name: "Acme Gooseberries", country: "GB" };
 
 // 1. Encapsulate the plain object in a instance
+// 2. Rename the internal field to use new field
+// 3. Add support for new field in constructor. Keeping compatibility with previous field
 class Organization {
-    private _name: string;
+    private _title: string;
     private _country: string;
 
     constructor(data: any) {
-        this._name = data.name;
+        this._title = data.title !== undefined ? data.title : data.name;
         this._country = data.country;
     }
 
-    get name() {
-        return this._name;
+    get title() {
+        return this._title;
     }
 
-    set name(newName: string) {
-        this._name = newName;
+    set title(newName: string) {
+        this._title = newName;
+    }
+
+    get country() {
+        return this._country;
+    }
+
+    set country(countryCode: string) {
+        this._country = countryCode;
     }
 }
+
+const organization = new Organization({
+    title: "Acme Gooseberries",
+    country: "GB",
+});
+
+console.log("Name:", organization.title);
+
+organization.title = "Google Inc.";
+
+console.log("Name:", organization.title);
