9d400ba0098923d9fe4b7d6aa6682428837b6f63
Jeferson Vinícius <jeferson.viniciuscrc@gmail.com>
2021-07-13 18:20:15 -0300

    Add pull up field example

---
+++ chapter-12/02-pull-up-field/01-before-refactoring.ts
@@ -0,0 +1,9 @@
+class Party {}
+
+class Employee extends Party {
+    partyMonthlyCost = 30;
+}
+
+class Department extends Party {
+    monthlyCostOfParty = 30;
+}
---
+++ chapter-12/02-pull-up-field/02-after-refactoring.ts
@@ -0,0 +1,7 @@
+class Party {
+    monthlyCostOfParty = 30;
+}
+
+class Employee extends Party {}
+
+class Department extends Party {}
